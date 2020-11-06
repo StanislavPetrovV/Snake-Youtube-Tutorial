49481a2148358ceb410b72ab53c7a60213557a3b
StanislavPetrovV <34447380+StanislavPetrovV@users.noreply.github.com>
2020-11-06 09:04:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -65,15 +65,15 @@ while True:
         if dirs['W']:
             dx, dy = 0, -1
             dirs = {'W': True, 'S': False, 'A': True, 'D': True, }
-    if key[pygame.K_s]:
+    elif key[pygame.K_s]:
         if dirs['S']:
             dx, dy = 0, 1
             dirs = {'W': False, 'S': True, 'A': True, 'D': True, }
-    if key[pygame.K_a]:
+    elif key[pygame.K_a]:
         if dirs['A']:
             dx, dy = -1, 0
             dirs = {'W': True, 'S': True, 'A': True, 'D': False, }
-    if key[pygame.K_d]:
+    elif key[pygame.K_d]:
         if dirs['D']:
             dx, dy = 1, 0
             dirs = {'W': True, 'S': True, 'A': False, 'D': True, }
